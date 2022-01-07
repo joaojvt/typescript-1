79a2af612913170c19794bee0668d475d5e12b9f
joaojvt <joao.jvts@gmail.com>
2022-01-07 05:51:49 -0300

    adicionando enums

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { DiasDaSemana } from "../enums/dias-da-semana.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
@@ -32,7 +33,8 @@ export class NegociacaoController {
   }
 
   private ehDiaUtil(data: Date): boolean {
-    return !(data.getDay() === 0 || data.getDay() === 6)
+    return !(data.getDay() === DiasDaSemana.DOMINGO
+      || data.getDay() === DiasDaSemana.SABADO)
   }
 
   private criaNegociacao(): Negociacao {
